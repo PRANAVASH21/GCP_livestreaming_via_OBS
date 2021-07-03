cef4513b0514ddfd777a98a95267945d501b22ed
Pranav Aditya Shukla <59870082+PRANAVASH21@users.noreply.github.com>
2021-07-03 21:30:37 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded());
+app.post("/auth", function (req, res){
+  /* This server is only available to nginx */
+  const streamkey = req.body.key ;
+  /* You can make database of users instead : ) */
+  if (streamkey === "supersecret") {
+    res.status(200).send();
+    return;
+  }
+  /* reject the stream */
+  res.status(403).send();
+  });
+  app.listen(8000, function (){
+    console.log("listening to port 8000!");
+  });
